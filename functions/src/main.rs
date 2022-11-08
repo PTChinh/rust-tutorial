2c8276adfee070b5253d96c45501d46f23463514
PTChinh <9a2.phamtranchinh@gmail.com>
2022-11-08 14:09:15 +0700

    Handle functions sample

---
+++ functions/src/main.rs
@@ -0,0 +1,33 @@
+fn main() {
+    another_function(5);
+
+    print_labeled_measurement(5, 'h');
+
+    let x = five();
+    let y = five_char();
+    let z = plus_one(5);
+
+    println!("The value of x is {x}");
+    println!("The value of y is {y}");
+    println!("The value of z is {z}");
+}
+
+fn another_function(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn five_char() -> char {
+    '5'
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
